2e26c7cbc127354fb9ff748f4225cc0a16b7ea14
Pamela <github email address>
2017-10-12 18:21:06 -0400

    venue controller added

---
+++ app/controllers/api/venues_controller.rb
@@ -0,0 +1,23 @@
+class Api::VenuesController < ApplicationController
+
+  before_action :set_venue, only: [:show]
+
+  def index
+    render json: Venue.all
+  end
+
+  def show
+    render json: @venue
+  end
+
+  private
+
+  def set_venue
+    @venue = Venue.find_by(id: params[:id])
+  end
+
+  def venue_params
+    params.require(:venue).params(:name, :city, :state, :description)
+  end
+
+end
